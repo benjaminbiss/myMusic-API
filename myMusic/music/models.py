734d209b46da10f0bc1be999ba8e9915d9977aa3
Ben Bissett <benjaminbissett@gmail.com>
2021-11-01 10:51:44 -0600

    Added the genre category in to the song model.

--- myMusic/music/models.py
+++ myMusic/music/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
+from django.db.models.fields import CharField
 
 class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
     release_date = models.DateTimeField()
+    genre = CharField(max_length=50)
