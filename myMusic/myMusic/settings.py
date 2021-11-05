b08b1afbccc16de16cef79f1db741ca5c50fbe12
Ben Bissett <benjaminbissett@gmail.com>
2021-11-05 09:48:16 -0600

    Added cors to the project.

--- myMusic/myMusic/settings.py
+++ myMusic/myMusic/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music'
+    'music',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'myMusic.urls'
@@ -115,3 +117,5 @@ try:
     from myMusic.local_settings import *
 except ImportError:
     pass
+
+CORS_ORIGIN_ALLOW_ALL=True
